2b9efdc06569961c427e40ed529bbc7e5b8c2b86
Nachichuri <nachichuri@gmail.com>
2023-04-14 14:45:55 -0300

    feat: change footer color to the same color selected for header

--- content.js
+++ content.js
@@ -22,6 +22,7 @@ window.addEventListener("load", function () {
     hostname.includes("health.aws.amazon.com")) {
     // AWS Console (including PHD)
     changeConsoleHeader();
+    changeConsoleFooter();
   }
 });
 
@@ -206,6 +207,66 @@ function changeConsoleHeader() {
             re = new RegExp(regexp);
             if (re.test(accountName)) {
               header.style.backgroundColor = colors[regexp];
+              return;
+            }
+          }
+        });
+      });
+    });
+  });
+}
+
+function changeConsoleFooter() {
+  // Change footer color
+  chrome.runtime.sendMessage({ method: "getSSOData" }, function (response) {
+    if (!(response && response.data)) {
+      return;
+    }
+    const accountMap = response.data;
+    const labelSelector = () =>
+      document
+        .querySelector("span[data-testid='awsc-nav-account-menu-button']")
+        .querySelector("span");
+    onElementReady(labelSelector, function (err, label) {
+      if (err) {
+        // console.warn("Ending SSO title update attempts.");
+        return;
+      }
+
+      const accountIdDiv = document
+        .querySelector("div[data-testid='account-detail-menu']")
+        .querySelectorAll("span");
+
+      var accountId = "";
+      const isNumberRegexp = new RegExp(/^[0-9]+(\.[0-9]+)?$/);
+      for (span of accountIdDiv) {
+        const accountIdTmp = span.innerText.replaceAll("-", "");
+        if (isNumberRegexp.test(accountIdTmp) && accountIdTmp.length == 12) {
+          accountId = accountIdTmp;
+          break;
+        }
+      }
+      if (!accountId) {
+        return;
+      }
+
+      const accountName = accountMap[accountId];
+
+      const footerSelector = () =>
+        document.querySelector("div[id='console-nav-footer-inner']");
+      onElementReady(footerSelector, function (err, footer) {
+        if (err) {
+          return;
+        }
+        chrome.storage.sync.get("ce_aws_sso_colors", function (items) {
+          var colors = defaultcolorjson;
+          if (items.ce_aws_sso_colors) {
+            colors = items.ce_aws_sso_colors;
+          }
+          for (var regexp in colors) {
+            re = new RegExp(regexp);
+            if (re.test(accountName)) {
+              footer.style.backgroundColor = colors[regexp];
               return;
             }
           }
